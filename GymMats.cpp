16f8d599ae8546b28da4ac98a654b542dcb6bee7
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-02-11 15:54:39 -0500

    Add files via upload

---
+++ GymMats.cpp
@@ -0,0 +1,18 @@
+// Gym Mats
+#include <iostream>
+using namespace std;
+
+
+// This function should calculate the area of a rectangle using
+// the parameters of width and length.
+double calculateArea(double length, double width) {
+	//complete the function
+}
+
+
+int main()
+{
+
+
+	system("pause");
+}
