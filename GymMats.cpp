867b2391d94d3c2c508001a87fadc1fc31aede8b
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2023-02-16 15:11:35 -0500

    Update GymMats.cpp

--- GymMats.cpp
+++ GymMats.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 
-// This function should calculate the area of a rectangle using
-// the parameters of width and length.
-double calculateArea(double length, double width) {
-	//complete the function
-}
+// Write a function that takes the length and width of a rectangle as parameters.
+// Compute and return the area.
+
 
 // Write a function called divide that takes 2 parameters.
 // The function should divide the parameters and return the result.
